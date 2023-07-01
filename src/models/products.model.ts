b1a6a1f703f5723f5bf9dcd945a0a50ec82f38ac
David Sanchez <davidendubai@gmail.com>
2023-06-30 20:32:18 -0500

    [+] products.model: define estructura de la entidad

---
+++ src/models/products.model.ts
@@ -0,0 +1,32 @@
+//  lA esctructura que definamos aqui se vera reflejada en nuestra base de datos
+import { Schema, model } from "mongoose";
+
+// Define estructura en la base de datos requerida por mongoose
+
+const productSchema = new Schema(
+
+    // este objeto principal definira atributos del modelo
+    {
+        name: {
+            type: String,
+            required: true
+        },
+        price: {
+            type: Number,
+            required: false
+        },
+        description:{
+            type: String
+
+        }
+    },
+    // Definira configuraciones que se pueden aplicar en Mongoose para ese objeto
+    {
+        timestamps: true 
+    }
+);
+
+// define el modelo a partir de la estructura requerida por mongoose
+const productModel = model( 'Products', productSchema  )
+
+export default productModel;
