a4c688d19fcdd433290155018be2cb556d846b8c
David Sanchez <davidendubai@gmail.com>
2023-07-11 21:48:15 -0500

    [+] Restringir el acceso a las rutas de producto

--- src/routes/products.route.ts
+++ src/routes/products.route.ts
@@ -6,14 +6,18 @@ import { authenticationUser } from "../middlewares/authentication.middleware";
 
 const router = Router();
 // http://localhost:3000/api/products/
-router.get('/', authenticationUser, getProducts);
+router.get('/', getProducts);
 router.get( '/', getProducts );       //oBTIENE LISTA DE TODOS LOS PRODUCTOS
 router.get ( '/:id', getProduct );    // Otiene produto por ID
-router.post('/', createProduct );     // Crea producto
-router.put('/:id', updateProduct );    // Actualiza todos los campos del productos
-router.patch('/:id', partialUpdateProduct ); //Actualiza algunos productos
-router.delete('/:id', deleteProduct );      // Elimina el producto
-
+router.post('/', authenticationUser, createProduct );     // Crea producto
+router.put('/:id', authenticationUser, updateProduct,  );    // Actualiza todos los campos del productos
+router.patch('/:id', authenticationUser, partialUpdateProduct ); //Actualiza algunos productos
+router.delete('/:id', authenticationUser, deleteProduct );      // Elimina el producto
+router.get( '/user/:id', authenticationUser, ( req: Request, res: Response ) =>{
+    res.json({
+        msj: 'TODO: Obtener todos los productos por usuario'
+    })
+} )
 
 export default router;
 
